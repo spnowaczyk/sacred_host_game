d4b17ed86ed2c3683b55d24a33ce47b773cca235
spnowaczyk <156540992+spnowaczyk@users.noreply.github.com>
2024-03-11 08:54:02 +0100

    Proxy

---
+++ cmake-build-debug/CMakeFiles/sacred_host.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sacred_host.dir/CharacterGO.cpp.obj"
+  "CMakeFiles/sacred_host.dir/CharacterGO.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/FadingTextBox.cpp.obj"
+  "CMakeFiles/sacred_host.dir/FadingTextBox.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/Game.cpp.obj"
+  "CMakeFiles/sacred_host.dir/Game.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/GameObject.cpp.obj"
+  "CMakeFiles/sacred_host.dir/GameObject.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/Map.cpp.obj"
+  "CMakeFiles/sacred_host.dir/Map.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/MouseController.cpp.obj"
+  "CMakeFiles/sacred_host.dir/MouseController.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/ObjectManager.cpp.obj"
+  "CMakeFiles/sacred_host.dir/ObjectManager.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/Pinpoint.cpp.obj"
+  "CMakeFiles/sacred_host.dir/Pinpoint.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/SFX.cpp.obj"
+  "CMakeFiles/sacred_host.dir/SFX.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/TextBox.cpp.obj"
+  "CMakeFiles/sacred_host.dir/TextBox.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/TextManager.cpp.obj"
+  "CMakeFiles/sacred_host.dir/TextManager.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/TextureManager.cpp.obj"
+  "CMakeFiles/sacred_host.dir/TextureManager.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/VisualEffect.cpp.obj"
+  "CMakeFiles/sacred_host.dir/VisualEffect.cpp.obj.d"
+  "CMakeFiles/sacred_host.dir/main.cpp.obj"
+  "CMakeFiles/sacred_host.dir/main.cpp.obj.d"
+  "libsacred_host.dll.a"
+  "sacred_host.exe"
+  "sacred_host.exe.manifest"
+  "sacred_host.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sacred_host.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
